b69ec4c961bbeba1b30341d3084728ba92737efe
GenadiyR <genadiy@sense.ly>
2018-06-05 21:24:49 +0300

    Fixed "lack of Consumer.swift"

---
+++ SenselySDKSample/SenselySDKSample/ConsumerScreen.swift
@@ -0,0 +1,31 @@
+//
+//  ConsumerScreen.swift
+//  SenselySDKTest
+//
+//  Created by Gennadiy Ryabkin on 5/22/18.
+//  Copyright © 2018 Sensely. All rights reserved.
+//
+
+import UIKit
+import Chat_sensely
+
+class ConsumerScreen: UIViewController {
+    
+    var avatarModule: AvatarModule?
+    var senselyCallback: CallbackData?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func getBackToSensely(_ sender: Any) {
+        
+        self.navigationController?.popViewController(animated: true)
+        
+        guard let callback = senselyCallback else {
+            print("Cann't continue sensely without CallbackData")
+            return
+        }
+        avatarModule?.resultOfInvokeCallback(callback)
+    }
+}
