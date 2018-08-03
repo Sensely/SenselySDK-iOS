3be991aae42e736edc141aecb21a21ddba038da4
Egor Afanasenko <dev.afanasenko@gmail.com>
2018-08-03 14:55:09 +0300

    fixed crash

--- SenselySDKSample/SenselySDKSample/AppDelegate.swift
+++ SenselySDKSample/SenselySDKSample/AppDelegate.swift
@@ -17,8 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        Configuration.readFromInfoPlist()
+        Configuration.senselyDomainType = .us
         return true
     }
 
