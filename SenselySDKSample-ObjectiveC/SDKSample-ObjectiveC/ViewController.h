49aa0de6608c7eab7c0fa6cbc3e014f9c3948940
Gennadiy Ryabkin <g.rjabkin@gmail.com>
2019-02-13 16:20:37 -0800

    Added Objective-C sample

---
+++ SenselySDKSample-ObjectiveC/SDKSample-ObjectiveC/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SDKSampleObjective-C
+//
+//  Created by Gennadiy Ryabkin on 2/13/19.
+//  Copyright © 2019 Sense.ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Chat_sensely/Chat_sensely.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SenselySDKSample-ObjectiveC/SDKSample-ObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SDKSample-ObjectiveC
+//
+//  Created by Gennadiy Ryabkin on 2/13/19.
+//  Copyright © 2019 Sense.ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
